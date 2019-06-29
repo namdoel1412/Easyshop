acaf472177ebd1351266c1cf62c265b0e5087d75
namdoel1412 <namdoel1412@gmail.com>
2019-06-29 21:09:14 +0700

    the first commit

---
+++ EasyShop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasyShop.Web.Startup))]
+namespace EasyShop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
